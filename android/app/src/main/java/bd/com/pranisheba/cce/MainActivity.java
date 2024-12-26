cc81033055f3cfebcae387b8f466248bea58199a
r-fine <saiadul.arfain@tuta.io>
2024-12-26 22:36:59 +0600

    Feat: Added pull-to-refresh feature and fixed back-button feature

--- android/app/src/main/java/bd/com/pranisheba/cce/MainActivity.java
+++ android/app/src/main/java/bd/com/pranisheba/cce/MainActivity.java
@@ -1,5 +1,44 @@
 package bd.com.pranisheba.cce;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
+        webView = findViewById(R.id.webView);
+
+        // Enable JavaScript
+        webView.getSettings().setJavaScriptEnabled(true);
+        
+        // Load the desired URL
+        webView.loadUrl("https://apscce.eu.pythonanywhere.com/");
+
+        swipeRefreshLayout.setOnRefreshListener(() -> webView.reload());
+
+        webView.setWebViewClient(new android.webkit.WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                swipeRefreshLayout.setRefreshing(false);
+                super.onPageFinished(view, url);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
